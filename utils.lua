f885061ecc86785979da41c3dba7dafb7e04e7bd
Rabidax <Rabidax@users.noreply.github.com>
2024-03-22 16:44:20 +0100

    feat: better utils

--- utils.lua
+++ utils.lua
@@ -1,14 +1,25 @@
 ---Utils
 local utils = {}
 
+---Return x if it is between 0 and limit
+---Else returns the closest of the two
+---@param x number
+---@param limit number
+---@return number
 function utils.clamp(x, limit)
 	return math.min(math.max(0, x), limit)
 end
 
+---@return boolean
 function utils.isInside(x, y, limitX, limitY)
 	return (x >= 0 and x <= limitX) and (y >= 0 and y <= limitY)
 end
 
+---Map all values in t to their image through f
+---Keeps the keys from t
+---@param t table
+---@param f function
+---@return table
 function utils.map(t, f)
 	local res = {}
 	for key, value in pairs(t) do
@@ -17,15 +28,56 @@ function utils.map(t, f)
 	return res
 end
 
-function utils.all(t, f)
-	local res = true
-	f = f or function(v)
-		return v
+---Accumulate the values in a table given an op and a start value
+---@generic T
+---@param t table<any, T>
+---@param op function(a:T,b:T):T
+---@param start T
+---@return T
+function utils.accum(t, op, start)
+	if not start then
+		error("nil initial value")
 	end
+	local res = start
 	for _, v in pairs(t) do
-		res = res and f(v)
+		res = op(res, v)
 	end
 	return res
+end
+
+---Return true if all the elements in a table are trueish
+---@param t table
+function utils.all(t)
+	return utils.accum(t, function(a, b)
+		return a and b
+	end, true)
+end
+
+---Sum the values in a table
+---@generic T
+---@param t table<any, T>
+---@param start T
+---@return T
+function utils.sum(t, start)
+	return utils.accum(t, function(a, b)
+		return a + b
+	end, start)
+end
+
+---Pick a random element from t
+---@param t table
+---@return any
+function utils.pick(t)
+	if #t == 0 then
+		error("list of length 0")
+	elseif #t == 1 then
+		return t[1]
+	end
+	for _, v in pairs(t) do
+		if math.random() < 1 / #t then
+			return v
+		end
+	end
 end
 
 return utils
